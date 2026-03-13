d76ebf88b2bdc1a9afd21d58b372abe4dddc5d92
Pragati Bavsiakr <pragatibaviskar@gtasterix.com>
2026-03-13 17:30:00 +0530

    Initial commit: Hospital backend with JWT security

---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,21 @@
+package com.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+}
---
+++ src/main/java/com/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
